3994b6345c4153ca029acf64aec708f2b48235b5
Jason Brownlee <jason.brownlee05@gmail.com>
2011-12-01 09:13:08 +1100

    started lda example

--- src/algorithms/regression/mass_linear_discriminant_analysis.R
+++ src/algorithms/regression/mass_linear_discriminant_analysis.R
@@ -1,20 +1,16 @@
-# stats_ordinary_least_squares.R
+# mass_linear_discriminant_analysis.R
 # 
 # The Clever Algorithms Project: http://www.CleverAlgorithms.com
 # (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
+# you may have to install the MASS package before you work with it
+# install.packages("MASS")
+
+# load the MASS package
+library(MASS)
+
 # define test data
 x <- runif(50, 0, 10)
 y <- x + rnorm(25)
 data <- data.frame(x, y)
-
-# linear model using ordinary least squares
-model = lm(y~x, data)
-# summarize the results
-summary(model)
-
-# plot the data
-plot(data)
-# plot the line of best fit
-abline(model$coef, lty=5)
