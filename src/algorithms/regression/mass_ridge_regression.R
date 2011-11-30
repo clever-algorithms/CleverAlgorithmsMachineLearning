77c9bbacab119b0cb5a2c01014734587c1eb8d8b
Jason Brownlee <jason.brownlee05@gmail.com>
2011-11-30 15:37:17 +1100

    ridge regression example

--- src/algorithms/regression/mass_ridge_regression.R
+++ src/algorithms/regression/mass_ridge_regression.R
@@ -10,3 +10,17 @@
 # load the MASS package
 library(MASS)
 
+# define sample data where most features are related to x
+x <- runif(50, 0, 10)
+y <- 2*x + rnorm(50)
+a <- 3*x - rnorm(50)
+b <- 6*x + rnorm(50)
+data <- data.frame(x, y, a, b)
+
+# perform a ridge regression with a large sequence of lambda values
+model <- lm.ridge(y~x+a+b, data, lambda=seq(0, 0.1, 0.001))
+
+# display the HKB, L-W, and GCV selected values of lambda
+select(model)
+# plot HKB, L-W, and GCV lambda values against the cost function
+plot(model)
