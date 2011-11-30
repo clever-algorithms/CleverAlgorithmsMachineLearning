1f5949f0dee9a5c798ff9c26fb49628f055dfac5
Jason Brownlee <jason.brownlee05@gmail.com>
2011-11-30 14:30:04 +1100

    started ridge regression

---
+++ src/algorithms/regression/mass_ridge_regression.R
@@ -0,0 +1,12 @@
+# mass_ridge_regression.R
+# 
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+# you may have to install the MASS package before you work with it
+# install.packages("MASS")
+
+# load the MASS package
+library(MASS)
+
