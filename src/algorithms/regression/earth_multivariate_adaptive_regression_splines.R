557f2d481b0cecff0a0d504dcceed0ac00abc802
Jason Brownlee <jason.brownlee05@gmail.com>
2011-12-02 09:18:26 +1100

    started mars

---
+++ src/algorithms/regression/earth_multivariate_adaptive_regression_splines.R
@@ -0,0 +1,17 @@
+# mda_mars.R
+# 
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+# you may have to install the mda package before you work with it
+# install.packages("earth")
+
+# load the MASS package
+library(earth)
+
+# define the data
+
+# create the Multivariate Adaptive Regression Splines model
+
+# plot
