49e75db1900795349063ac69de2f5f8abd19de7c
Jason Brownlee <jason.brownlee05@gmail.com>
2011-12-01 14:21:58 +1100

    basic lda example

--- src/algorithms/regression/mass_linear_discriminant_analysis.R
+++ src/algorithms/regression/mass_linear_discriminant_analysis.R
@@ -13,8 +13,17 @@ library(MASS)
 # define test data with classes 0 and 1
 x <- c(runif(25, 0, 5), runif(25, 6, 10))
 y <- c(x+rnorm(25))
-z <- c(rep(0, 25), rep(1, 25))
+z <- c(rep("a", 25), rep("b", 25))
 data <- data.frame(x, y, z)
 
+# perform the linear discriminant analysis
+model <- lda(z~x+y, data=data)
+
+# display model details
+model
+
 # plot the data
-plot(data)
+plot(x,y)
+# plot the class means
+points(model$means[1], model$means[3], col="black", pch=19)
+points(model$means[2], model$means[4], col="black", pch=19)
