df8cec2b6032451022784880e6bf9b72bc40f381
Jason Brownlee <jason.brownlee05@gmail.com>
2011-12-07 17:49:17 +1100

    added mds example

---
+++ src/algorithms/dimension/mass_multidimensional_scaling.R
@@ -0,0 +1,27 @@
+# mass_multidimensional_scaling.R
+# 
+# The Clever Algorithms Project: http://www.CleverAlgorithms.com
+# (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
+# This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
+
+# you may have to install the MASS package before you work with it
+install.packages("MASS")
+# load the MASS package
+library(MASS)
+
+# define a function that creates a 3d dataset
+dataset <- function() {   
+	x <- c(rnorm(50, mean=0), rnorm(50, mean=4))
+	y <- c(rnorm(50, mean=4), rnorm(50, mean=8))
+	z <- c(rnorm(50, mean=8), rnorm(50, mean=10))
+	data <- data.frame(x, y, z)
+}
+
+# get the data 
+data <- dataset()
+# create a distance matrix
+distances <- dist(data)
+# construct a model using sammon mapping
+model <- isoMDS(distances)
+# plot the model
+plot(model$points)
