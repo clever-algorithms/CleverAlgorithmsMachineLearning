23010d1e5ec987b93974e867437ed65f16fb120e
Jason Brownlee <jason.brownlee05@gmail.com>
2012-01-02 09:56:28 +1100

    working on regression

--- src/algorithms/regression/stats_ordinary_least_squares.R
+++ src/algorithms/regression/stats_ordinary_least_squares.R
@@ -4,11 +4,16 @@
 # (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-# define test data
-x <- runif(50, 0, 10)
-y <- x + rnorm(50)
-data <- data.frame(x, y)
+# define 2D regression problem of 50 samples
+regression <- function() {
+	x <- runif(50, 0, 10)
+	y <- x + rnorm(50)
+	data.frame(x, y)	
+}
 
+
+# prepare the dataset
+data <- regression()
 # linear model using ordinary least squares
 model = lm(y~x, data)
 # summarize the results
