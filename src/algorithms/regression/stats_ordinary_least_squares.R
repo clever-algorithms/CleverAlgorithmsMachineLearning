247ebc675170a4d895afff515a619de543134921
Jason Brownlee <jason.brownlee05@gmail.com>
2012-01-03 16:47:59 +1100

    first cut of OLS

--- src/algorithms/regression/stats_ordinary_least_squares.R
+++ src/algorithms/regression/stats_ordinary_least_squares.R
@@ -4,22 +4,39 @@
 # (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
-# define 2D regression problem of 50 samples
-regression <- function() {
-	x <- runif(50, 0, 10)
-	y <- x + rnorm(50)
+# define 2D regression problem of 100 samples
+regression <- function() {   
+	x <- runif(100, 0, 10)
+	y <- x + rnorm(100)
 	data.frame(x, y)	
 }
 
-
-# prepare the dataset
+# get the data 
 data <- regression()
-# linear model using ordinary least squares
-model = lm(y~x, data)
-# summarize the results
-summary(model)
+# split data in to train and test (67%/33%)
+training_set = sample(100,67)
+train <- data[training_set,]
+test <- data[(1:100)[-training_set],]
 
-# plot the data
-plot(data)
-# plot the line of best fit
-abline(model$coef, lty=5)
+# create a linear regression model using ordinary least squares
+model = lm(
+	y~x,					# predict Y given X
+	train,				# training dataset
+	NULL,					# no weighting on the variables
+	NULL,					# no action on missing values
+	method="qr")	# QR decomposition (efficient matrix calculation method)
+
+# summarize the create linear regression model
+summary(model)
+# regression model diagnostic plots with the training data
+par(mfrow=c(2,2))
+plot(model)
+
+# plot the training data and the line of best fit
+plot(train)
+abline(model$coef, lty=5, col="red")
+
+# make predictions for the test data
+predictions <- predict.lm(model, test[,1:2])
+# compute mean squared error
+print(mean((test$y-predictions)^2))
