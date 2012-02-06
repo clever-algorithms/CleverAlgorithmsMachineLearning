3a839a94e773432756dd63eada42ab1a9a9aacc4
Jason Brownlee <jason.brownlee05@gmail.com>
2012-02-06 13:51:17 +1100

    removed redundant files

--- workspace/cleanreport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-LOC=.
-
-rm $LOC/*.aux $LOC/*.bbl $LOC/*.blg $LOC/*.toc $LOC/*.pdf $LOC/*.out $LOC/*.log 
--- workspace/makereport.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-INPUT=$1
-
-pdflatex ${INPUT};true
-bibtex ${INPUT};true
-pdflatex ${INPUT};true
-pdflatex ${INPUT};true
-
