b1782e7cdef44e3e4e3866deee9238452328c375
Jason Brownlee <jason.brownlee05@gmail.com>
2013-01-07 08:37:53 +1100

    updated copyright

--- web/generate.rb
+++ web/generate.rb
@@ -2,7 +2,7 @@
 # just a big collection of hacks - never do this - use a real lexer/parser
 
 # The Clever Algorithms Project: http://www.CleverAlgorithms.com
-# (c) Copyright 2011 Jason Brownlee. Some Rights Reserved. 
+# (c) Copyright 2013 Jason Brownlee. Some Rights Reserved. 
 # This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 2.5 Australia License.
 
 require 'bibtex'
